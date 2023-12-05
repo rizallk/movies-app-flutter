77086d9b60d223eb8c5cf1894d2fd65495316062
dengolden <antoniomarquez1593@gmail.com>
2023-12-05 20:48:11 +0800

    add api_key & creating splash screen

---
+++ android/app/src/main/kotlin/com/example/cinemav_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cinemav_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = 'aa03fdad568905d099c878e4e7ce1aa5';
+}

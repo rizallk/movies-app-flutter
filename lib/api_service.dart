835a13ae7a0f3afd058b66e075268e1d8f69ecef
dengolden <antoniomarquez1593@gmail.com>
2023-12-06 02:58:11 +0800

    creating api service

---
+++ lib/api_service.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'app_constant.dart';
+
+class ApiService {
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: AppConstant.baseUrl,
+    queryParameters: {'api_key': AppConstant.apiKey},
+  ));
+
+  Future<Map<String, dynamic>> getDiscoverMovies() async {
+    try {
+      final response = await _dio.get('/discover/movie');
+
+      if (response.statusCode == 200) {
+        return json.decode(response.toString());
+      } else {
+        throw Exception('Failed to load movies');
+      }
+    } catch (error) {
+      throw Exception('Failed to load movies: $error');
+    }
+  }
+
+  Future<Map<String, dynamic>> getTopRatedMovies() async {
+    try {
+      final response = await _dio.get('/movie/top_rated');
+
+      if (response.statusCode == 200) {
+        return json.decode(response.toString());
+      } else {
+        throw Exception('Failed to load top rated movies');
+      }
+    } catch (error) {
+      throw Exception('Failed to load top rated movies: $error');
+    }
+  }
+
+  Future<Map<String, dynamic>> getPopularMovies() async {
+    try {
+      final response = await _dio.get('/movie/popular');
+
+      if (response.statusCode == 200) {
+        return json.decode(response.toString());
+      } else {
+        throw Exception('Failed to load popular movies');
+      }
+    } catch (error) {
+      throw Exception('Failed to load popular movies: $error');
+    }
+  }
+}

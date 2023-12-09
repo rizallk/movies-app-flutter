a83e8f2878eca1dd525eb48a16e121ac5baaf072
rizallk <rizallkadamong@gmail.com>
2023-12-09 13:45:15 +0800

    feat: add duration, rating, release date and genres:

--- lib/movie_model.dart
+++ lib/movie_model.dart
@@ -1,21 +1,32 @@
 class Movie {
   final int id;
+  final int duration;
+  final double rating;
   final String title;
   final String overview;
   final String posterPath;
+  final String releaseDate;
+  final List genres;
 
   Movie(
       {required this.id,
+      required this.duration,
+      required this.rating,
       required this.title,
       required this.overview,
-      required this.posterPath});
+      required this.posterPath,
+      required this.releaseDate,
+      required this.genres});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      id: json['id'],
-      title: json['title'],
-      overview: json['overview'],
-      posterPath: json['poster_path'],
-    );
+        id: json['id'],
+        duration: json['runtime'],
+        rating: json['popularity'],
+        title: json['title'],
+        overview: json['overview'],
+        posterPath: json['poster_path'],
+        releaseDate: json['release_date'],
+        genres: json['genres']);
   }
 }
